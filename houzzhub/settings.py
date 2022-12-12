76393a57b04680e407b55ed8b9b5463863688dcb
Abubkr Aslam <rabubakar991@gmail.com>
2022-12-12 16:44:34 +0500

    read_env function in manage.py file

--- houzzhub/settings.py
+++ houzzhub/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'houzzhub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'houz_db',
-        'USER': 'houz_user',
-        'PASSWORD': 'houz_pass',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
